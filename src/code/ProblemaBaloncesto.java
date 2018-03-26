b0c5dc8f9bcecdd6bd88494bc77825a80c57787b
Juan Arteaga Carmona <juan.arteaga41567@gmail.com>
2018-03-26 21:15:01 +0200

    That's enough for today, encodings are a pain in the butt

--- src/code/ProblemaBaloncesto.java
+++ src/code/ProblemaBaloncesto.java
@@ -16,47 +16,47 @@ import us.lsi.pl.AlgoritmoPLI;
 
 public class ProblemaBaloncesto {
 
-	public static void main(String[] args) {	
-		
+	public static void main(String[] args) {
+
 		Integer presupuesto = 10;
 		Integer seleccionarJugadores = 7;
-		
+
 		List<Jugador> jugadores = getJugadoresDesdeArchivo("suplentes.txt");
 
-		
-		
-		
+
+
+
 		//Inicializacion de la string
 		String r = "";
 		r = r+"max: ";
-		
-		//Funcion objetivo. Variables y pesos 
+
+		//Funcion objetivo. Variables y pesos
 		for(int i =0;i<jugadores.size();i++){
 			if (i!=0) r +=" + ";
 			r += jugadores.get(i).getValorCortos()+jugadores.get(i).getValorLargos() + "*x"+i;
 		}
 		r += ";\n\n";
-		
+
 		//Restricciones.
-		
+
 		//Suma de cache<= presupuesto
 				for (int i = 0; i < jugadores.size(); i++) {
 					if (i!=0) r = r+"+";
 					r += jugadores.get(i).getCache()+"*x" + i;
 				}
 				r+= " <= "+presupuesto+";\n";
-				
-		
-		
+
+
+
 		//Seleccionar solamente n jugadores
 		for (int i = 0; i < jugadores.size(); i++) {
 			if (i!=0) r = r+"+";
 			r += "x" + i;
 		}
 		r += " = "+seleccionarJugadores+";\n";
-		
-		
-		
+
+
+
 		//Solo un base
 		for (int i = 0; i < jugadores.size(); i++) {
 			if (jugadores.get(i).getPos1().equals("Base")|| jugadores.get(i).getPos2().equals("Base")) {
@@ -64,25 +64,25 @@ public class ProblemaBaloncesto {
 			}
 		}
 		r += "0 = 1;\n";
-		
-		
+
+
 		//Pivots
-		for (int i = 0; i < jugadores.size(); i++) {			
+		for (int i = 0; i < jugadores.size(); i++) {
 			if( jugadores.get(i).getPos1().equals("Pivot") || jugadores.get(i).getPos2().equals("Pivot")) {
 				r += "x"+i+"+";
 			}
 		}
 		r += "0 >= 2;\n";
-		
-		
+
+
 		//Aleros
-		for (int i = 0; i < jugadores.size(); i++) {			
+		for (int i = 0; i < jugadores.size(); i++) {
 			if( jugadores.get(i).getPos1().equals("Alero") || jugadores.get(i).getPos2().equals("Alero")) {
 				r += "x"+i+"+";
 			}
 		}
 		r += "0 >= 3;\n\nbin ";
-		
+
 		//Declaracion variables binarias
 		for (int i = 0; i < jugadores.size(); i++) {
 			r+="x"+i+" ";
@@ -90,7 +90,7 @@ public class ProblemaBaloncesto {
 		r+=";";
 		//--------------------------------------------------
 		//Guardar la string a un archivo
-		
+
 		try {
 			PrintWriter out = new PrintWriter("ArchivoLPSolveGenerado.txt");
 			out.print(r);
@@ -98,47 +98,47 @@ public class ProblemaBaloncesto {
 		} catch (Exception e) {
 			System.err.println("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!");
 		}
-		
-		
-		
+
+
+
 //-------------------------------------------------------------------
 
-		
+
 		AlgoritmoPLI a = Algoritmos.createPLI("ArchivoLPSolveGenerado.txt");
-		
+
 		List<String> NombresSolucion = new ArrayList<>();
-		
+
 		a.ejecuta();
-				
+
 		double[] solucion = a.getSolucion();
 		for (int i=0;i<solucion.length;i++) {
 			if (solucion[i]==1.) {
 				NombresSolucion.add(jugadores.get(i).getNombre());
 			}
 		}
-	
-		
+
+
 		//Impresiones por pantalla:
-		System.out.println("Se ha generado un archivo llamado \"ArchivoLPSolveGenerado.txt\" que contiene la definición de la solucion del problema en formato LPSolve");
-		System.out.println("\nUna vez ejecutado, esta es la solución al problema:");
+		System.out.println("Se ha generado un archivo llamado \"ArchivoLPSolveGenerado.txt\" que contiene la definiciÃ³n de la soluciÃ³n del problema en formato LPSolve");
+		System.out.println("\nUna vez ejecutado, esta es la soluciÃ³n al problema:");
 		System.out.println(NombresSolucion);
 	}
 
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 	public static List<Jugador> getJugadoresDesdeArchivo(String path){
 		List<Jugador> res = new ArrayList<>();
-	
+
 		try {
 			File archivo = new File(path);
 			FileReader fr = new FileReader(archivo);
 			BufferedReader br = new BufferedReader(fr);
-			
+
 			// Lectura del fichero
 	        String linea;
 	        String[] div = null;
@@ -147,12 +147,12 @@ public class ProblemaBaloncesto {
 				res.add(new Jugador(new Integer(div[0]), div[1],div[2],div[3],new Integer(div[4]),div[5],new Integer(div[6]),new Integer(div[7]),new Integer(div[8])));
 			}
 			fr.close();
-			
+
 		} catch (Exception e) {
 			System.err.println("OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!");
 		}
-        
+
 		return res;
 	}
-	
+
 }
